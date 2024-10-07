1cc2f90b5378c3eeafaba606006900b337846e86
Bruna Radeke <brunaradeke@brunas-mbp.home>
2024-10-07 23:06:11 +0100

    style ordering props - review point

--- src/app/style2/page.tsx
+++ src/app/style2/page.tsx
@@ -10,13 +10,13 @@ function Box({
   return (
     // className={`box ${className} ${sizeClassName}`.trim()}
     // Array [].filter(Boolean).join(' ') to remove extra spaces in between
-
+    // filter(boolean) removes false strings
     <div
+      style={{ fontStyle: "italic", ...otherprops.style }}
+      {...otherprops}
       className={["box", otherprops.className, sizeClassName]
         .filter(Boolean)
         .join(" ")}
-      style={{ fontStyle: "italic", ...otherprops.style }}
-      {...otherprops}
     />
   );
 }
